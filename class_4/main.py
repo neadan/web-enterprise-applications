192a3480c6afa2c4c1fc875aed8525e80dc14ad7
Tiberiu <danielt.neagoe@gmail.com>
2021-06-19 17:26:44 -0400

    Class 4 exercise solution

--- class_4/main.py
+++ class_4/main.py
@@ -1,8 +1,21 @@
 import wikipedia
 
-results = wikipedia.search("Barack")
-print(results)
+page = input("What page would you like to view?")
+word_to_count = input("Which word would you like to count?")
 
-obama_page = wikipedia.page("Barack Obama")
-print(obama_page.title)
-print(obama_page.content)
+desired_page = wikipedia.page(page)
+# url
+print(desired_page.url)
+
+# number of words
+content = desired_page.content
+split = content.split(" ")
+print(f"The number of words in the document is: {len(split)}")
+
+# count of specific word
+count = 0
+for word in split:
+    if word == word_to_count:
+        count = count + 1
+
+print(f"The word '{word_to_count}' shows up {count} times")
